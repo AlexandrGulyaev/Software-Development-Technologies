c87082292ed2419ac47b2b645345267f29cd33fe
Grand-Admiral Sanya <38395460+AlexandrGulyaev@users.noreply.github.com>
2020-04-01 19:17:03 +0300

    Add files via upload

---
+++ Figure.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gulyaev_AG_2
+{
+    abstract class Figure
+    {
+        public abstract string Type();
+        public abstract double Perimeter();
+        public abstract double Area();
+
+        /// <summary>
+        /// Вывод информации (тип, площадь, периметр) о фигуре
+        /// </summary>
+        public void ShowInfo()
+        {
+            Console.WriteLine(
+                $"Тип фигуры: {Type()}\n" +
+                $"Площадь: {Area()}\n" +
+                $"Периметр: {Perimeter()}"
+            );
+            Console.WriteLine();
+        }
+    }
+
+    class Rectangle : Figure
+    {
+        private double l; // Длина прямоугольника
+        private double h; // Высота прямоугольника
+
+        /// <summary>
+        /// Объявление прямоугольника
+        /// </summary>
+        /// <param name="recLength">Длина прямоугольника</param>
+        /// <param name="recHeight">Высота прямоугольника</param>
+        public Rectangle(double recLength, double recHeight)
+        {
+            l = recLength < 0 ? -recLength : recLength;
+            h = recHeight < 0 ? -recHeight : recHeight;
+        }
+
+        /// <summary>
+        /// Вычисление площади прямоугольника
+        /// </summary>
+        public override double Area()
+        {
+            return (l * h);
+        }
+
+        /// <summary>
+        /// Вычисление периметра прямоугольника
+        /// </summary>
+        public override double Perimeter()
+        {
+            return (2 * (l + h));
+        }
+
+        public override string Type()
+        {
+            return ("Rectangle");
+        }
+    }
+
+    class Circle : Figure
+    {
+        private double r; // Радиус окружности
+
+        /// <summary>
+        /// Объявление окружности
+        /// <param name="cirRadius">Радиус окружности</param>
+        /// </summary>
+        public Circle(double cirRadius)
+        {
+            r = cirRadius < 0 ? -cirRadius : cirRadius;
+        }
+
+        /// <summary>
+        /// Вычисление площади окружности
+        /// </summary>
+        public override double Area()
+        {
+            return (Math.PI * r * r);
+        }
+
+        /// <summary>
+        /// Вычисление периметра окружности
+        /// </summary>
+        public override double Perimeter()
+        {
+            return (2 * Math.PI * r);
+        }
+
+        public override string Type()
+        {
+            return ("Circle");
+        }
+    }
+
+    class Triangle : Figure
+    {
+        private double a, b, c; // Стороны треугольника
+
+        /// <summary>
+        /// Объявление треугольника по трем сторонам
+        /// <param name="trA">Первая сторона треугольника</param>
+        /// <param name="trB">Вторая сторона треугольника</param>
+        /// <param name="trC">Третья сторона треугольника</param>
+        /// </summary>
+        public Triangle(double trA, double trB, double trC)
+        {
+            a = trA < 0 ? -trA : trA;
+            b = trB < 0 ? -trB : trB;
+            c = trC < 0 ? -trC : trC;
+        }
+
+        /// <summary>
+        /// Вычисление площади треугольника
+        /// </summary>
+        public override double Area()
+        {
+            double p = (a + b + c) / 2; // Полупериметр
+            return (Math.Sqrt(p * (p - a) * (p - b) * (p - c)));
+        }
+
+        /// <summary>
+        /// Вычисление периметра треугольника
+        /// </summary>
+        public override double Perimeter()
+        {
+            return (a + b + c);
+        }
+
+        public override string Type()
+        {
+            return ("Triangle");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Gulyaev_AG_2
+{
+    class Program
+    {
+        /// <summary>
+        /// Создание ограниченного (по числу элементов) списка фигур
+        /// </summary>
+        /// <param name="n">Максимальная длина списка</param>
+        /// <returns>Список фигур</returns>
+        static List<Figure> CreateList(int n)
+        {
+            using (StreamReader sr = new StreamReader("input.txt"))
+            {
+                sr.ReadLine();
+                List<Figure> figures = new List<Figure>();
+                
+                for (int k = 0; k < n; k++)
+                {
+                    // Считывание строки с параметрами фигуры
+                    string line = sr.ReadLine();
+                    // Определение формы фигуры
+                    string form = line.Split()[0];
+                    // Добавление в список информации о соответствующей фигуре
+                    try
+                    {
+                        switch (form.ToLower())
+                        {
+                            case "rectangle":
+                                Rectangle rectangle = new Rectangle(double.Parse(line.Split()[1]), double.Parse(line.Split()[2]));
+                                figures.Add(rectangle);
+                                break;
+
+                            case "triangle":
+                                Triangle triangle = new Triangle(double.Parse(line.Split()[1]), double.Parse(line.Split()[2]), double.Parse(line.Split()[2]));
+                                figures.Add(triangle);
+                                break;
+
+                            case "circle":
+                                Circle circle = new Circle(double.Parse(line.Split()[1]));
+                                figures.Add(circle);
+                                break;
+                        }
+                    }
+                    catch (System.FormatException)
+                    {
+                        Console.WriteLine("Данные об одной из фигур некорректны.");
+                        Console.ReadKey();
+                        Environment.Exit(0);
+                    }
+                }
+                return figures;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            using (StreamReader sr = new StreamReader("input.txt"))
+            {
+                string line = sr.ReadLine();
+
+                try
+                {
+                    int n = Int32.Parse(line.Trim());
+                    
+                    // Создание списка фигур
+                    List<Figure> figures = CreateList(n);
+
+                    // Вывод информации о фигурах, если список полный
+                    if (figures.Count != n)
+                    {
+                        Console.WriteLine("Данные об одной из фигур не найдены, или название фигуры некорректно.");
+                    }
+                    else
+                    {
+                        for (int k = 0; k < n; k++)
+                        {
+                            figures[k].ShowInfo();
+                        }
+                    }
+                }
+                catch (System.FormatException)
+                {
+                    Console.WriteLine("Введенное количество фигур некорректно.");
+                    Console.ReadKey();
+                    Environment.Exit(0);
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
